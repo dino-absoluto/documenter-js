23d5d894af03e618dee502576cf625787af2330e
Dino <dinoabsoluto+dev@gmail.com>
2019-05-05 18:46:48 +0700

    Add renderer

--- src/documenter.ts
+++ src/documenter.ts
@@ -17,16 +17,36 @@
  *
  */
 /* imports */
-import { ApiModel } from '@microsoft/api-extractor-model'
+import { ApiModel, ApiItem, ApiDocumentedItem } from '@microsoft/api-extractor-model'
+import { format } from 'util'
+import render from './renderer'
 /* code */
+
+const traverse = (item: ApiItem, depth = 0): void => {
+  let text = format(
+    depth,
+    item.kind, item.displayName
+  )
+  if (item instanceof ApiDocumentedItem && item.tsdocComment) {
+    const docComment = item.tsdocComment
+    text += '\n---render\n' + format(
+      render(docComment.summarySection)
+    )
+    if (docComment.remarksBlock) {
+      text += render(docComment.remarksBlock)
+    }
+    text += '\n---render-end'
+  }
+  console.log(text)
+  for (const mem of item.members) {
+    traverse(mem, depth + 1)
+  }
+}
 
 export const documenter = (modelFile: string): void => {
   const model = new ApiModel()
   const pkg = model.loadPackage(modelFile)
   for (const entry of pkg.members) {
-    for (const mem of entry.members) {
-      console.log(mem.kind, mem.displayName)
-      console.log(mem)
-    }
+    traverse(entry)
   }
 }
---
+++ src/renderer/index.ts
@@ -0,0 +1,51 @@
+/**
+ * @author Dino <dinoabsoluto+dev@gmail.com>
+ * @license
+ * Copyright 2019 Dino <dinoabsoluto+dev@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+/* imports */
+import {
+  DocNodeKind,
+  DocNode,
+  DocPlainText,
+  DocBlock
+} from '@microsoft/tsdoc'
+
+/* code */
+export const render = (node: DocNode): string => {
+  switch (node.kind) {
+    case DocNodeKind.PlainText: {
+      const typed = node as DocPlainText
+      return typed.text
+    }
+    case DocNodeKind.SoftBreak: {
+      return '\n'
+    }
+    case DocNodeKind.Paragraph:
+    case DocNodeKind.Section: {
+      return node.getChildNodes().map(render).join('')
+    }
+    case DocNodeKind.Block: {
+      const typed = node as DocBlock
+      return `## ${typed.blockTag.tagName}\n${render(typed.content)}`
+    }
+    default: {
+      return node.kind
+    }
+  }
+}
+
+export default render
