eeee6709d3a31173675b0dbea71cb7cc507d8e49
Dino <dinoabsoluto+dev@gmail.com>
2019-05-06 17:14:03 +0700

    Generate IDs

---
+++ src/ast/__tests__/document.spec.ts
@@ -0,0 +1,48 @@
+/**
+ * @author Dino <dinoabsoluto+dev@gmail.com>
+ * @license
+ * Copyright 2019 Dino <dinoabsoluto+dev@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+/* imports */
+import { ParentNode } from '../node'
+import { Document } from '../document'
+import { Heading } from '../heading'
+
+/* code */
+describe('Document', () => {
+  test('setPath()', () => {
+    const doc = new Document()
+    const group = new ParentNode()
+    const h1 = new Heading('example')
+    const h2 = new Heading('example')
+    const h3 = new Heading('Hello World!')
+    doc.append(group)
+    doc.append(h1)
+    group.append(h2)
+    doc.append(h3)
+    expect(h1.link).toBe(undefined)
+    expect(h2.link).toBe(undefined)
+    expect(h3.link).toBe(undefined)
+    doc.setPath('simple.md')
+    expect(h1.link).toBe('simple.md#example-1')
+    expect(h2.link).toBe('simple.md#example')
+    expect(h3.link).toBe('simple.md#hello-world')
+    doc.setPath()
+    expect(h1.link).toBe(undefined)
+    expect(h2.link).toBe(undefined)
+    expect(h3.link).toBe(undefined)
+  })
+})
---
+++ src/ast/document.ts
@@ -0,0 +1,65 @@
+/**
+ * @author Dino <dinoabsoluto+dev@gmail.com>
+ * @license
+ * Copyright 2019 Dino <dinoabsoluto+dev@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+/* imports */
+import { ChildNode, ParentNode } from './node'
+import { Heading } from './heading'
+import toId from '../utils/to-id'
+/* code */
+
+export class Document extends ParentNode {
+  private pPath?: string
+  private static * traverse (node: ChildNode): IterableIterator<ChildNode> {
+    yield node
+    if (node instanceof ParentNode) {
+      for (const cnode of node.children) {
+        yield * Document.traverse(cnode)
+      }
+    }
+  }
+
+  public setPath (fpath?: string): void {
+    this.pPath = fpath
+    this.generateIDs()
+  }
+  public generateIDs (): void {
+    const { pPath: fpath } = this
+    const idCount: { [id: string]: number } = {}
+    if (fpath === undefined) {
+      for (const node of Document.traverse(this)) {
+        if (node instanceof Heading) {
+          node.link = undefined
+        }
+      }
+      return
+    }
+    for (const node of Document.traverse(this)) {
+      if (node instanceof Heading) {
+        const id = toId(node.text)
+        let count = 0
+        if (idCount[id] === undefined) {
+          idCount[id] = 1
+        } else {
+          count = idCount[id]++
+        }
+        node.link = `${fpath}#${toId(node.text)}` +
+          (count > 0 ? ('-' + count) : '')
+      }
+    }
+  }
+}
