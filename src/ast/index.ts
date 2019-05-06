efbf284832e0f828d5a446a91eec3e95a878400a
Dino <dinoabsoluto+dev@gmail.com>
2019-05-07 05:12:30 +0700

    Add ast/index.ts

---
+++ src/ast/index.ts
@@ -0,0 +1,35 @@
+/**
+ * @author Dino <dinoabsoluto+dev@gmail.com>
+ * @license
+ * Copyright 2019 Dino <dinoabsoluto+dev@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+/* reexports */
+export { CodeBlock } from './code-block'
+export { CodeSpan } from './code-span'
+export { Document } from './document'
+export { Heading } from './heading'
+export {
+  Reference,
+  Link,
+  Image
+} from './link'
+export { Node, Span, Block } from './node'
+export {
+  Table,
+  TableRow,
+  TableHeader,
+  TableCell
+} from './table'
